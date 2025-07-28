239b4387618da3ee9a63c9de8062f5b201e43c02
Anitesh <aniteysh@gmail.com>
2025-07-28 15:23:05 +0000

    added new methods

--- Day-02/practice/Advanced-String-Manipulation.py
+++ Day-02/practice/Advanced-String-Manipulation.py
@@ -1,7 +1,19 @@
-input "Hello Hello World. This is a fantastic world"
+input = "Hello Hello World. This is a fantastic world"
 
 def number_of_words(input):
     words = input.split()
-    return words
+    return len(words)
 
-print (number_of_words)
+def unique_words(input):
+    words = input.split()
+    unique = set(words)
+    return len(unique)
+
+def most_common_words(input):
+    array = {}
+    words  = input.split()
+    for word in words:
+        if(array[]) 
+
+print (number_of_words(input))
+print (unique_words(input))
