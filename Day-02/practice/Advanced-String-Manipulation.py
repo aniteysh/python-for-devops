cc3430eaca866e5008e909f648f95272e8003300
aniteysh <aniteysh@gmail.com>
2025-07-28 20:56:44 +0530

    completed most_common_words method

--- Day-02/practice/Advanced-String-Manipulation.py
+++ Day-02/practice/Advanced-String-Manipulation.py
@@ -13,7 +13,12 @@ def most_common_words(input):
     array = {}
     words  = input.split()
     for word in words:
-        if(array[]) 
+        if( word in array):
+            array[word] += 1
+        else:
+            array[word] = 1
+    return array 
 
 print (number_of_words(input))
 print (unique_words(input))
+print (most_common_words(input))
